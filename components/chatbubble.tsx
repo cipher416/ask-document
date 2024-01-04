abe87e1af65847659e7505b3bc2c3768e5bacf2e
Cristoper Anderson <cristoper.anderson@binus.ac.id>
2024-01-04 13:52:50 +0700

    fix: profile image sizing

--- components/chatbubble.tsx
+++ components/chatbubble.tsx
@@ -1,5 +1,5 @@
 'use client'; 
-import { Avatar } from "@radix-ui/react-avatar";
+import { Avatar } from "./ui/avatar";
 import { AvatarImage } from "./ui/avatar";
 
 import type { Message } from "ai/react";
@@ -22,8 +22,8 @@ export default function ChatBubble({message}: ChatBubbleProps) {
             //  : ''
             }/> 
         </Avatar>
-        <div className="inline-block align-middle">
-          asdfasfd
+        <div className="inline-block align-middle max-w-[50vw]">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
         </div>
       </div>
       <Separator/>
