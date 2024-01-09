f24ec5e0e54bee6a41116bb2661743037570f70a
Cristoper Anderson <cristoper.anderson@binus.ac.id>
2024-01-09 13:49:51 +0700

    fix: change prisma config

--- lib/options.ts
+++ lib/options.ts
@@ -2,9 +2,10 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./db";
 import Auth0Provider from "next-auth/providers/auth0";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaClient } from "@prisma/client";
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma), 
+  adapter: PrismaAdapter(prisma as PrismaClient), 
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID!,
