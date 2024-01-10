9058f6b88f19c638f0e6a41c4dbfdc1e1c7e2f88
Cristoper Anderson <cristoper.anderson@binus.ac.id>
2024-01-10 13:17:49 +0700

    fix: add workerSrc to pdfjs import

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export async function getTextFromPDF(path: string) {
   const pdfjs = await import('pdfjs-dist/legacy/build/pdf.mjs');
+  pdfjs.GlobalWorkerOptions.workerSrc = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.7.570/build/pdf.worker.min.js";
   let doc = await pdfjs.getDocument(path).promise;
   let pages = doc.numPages;
   let pageContents = '';
